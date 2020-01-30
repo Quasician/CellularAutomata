c3dfa5966c58f29053bcc29cf0c66dd6568eae64
Rodrigo <rea20@duke.edu>
2020-01-29 21:04:01 -0500

    added constructor to simulation class with row,col,width, and height variables. Also added get functions for each of those methods and two potentional update methods.

--- src/cellsociety/Simulation.java
+++ src/cellsociety/Simulation.java
@@ -1,4 +1,30 @@
 package cellsociety;
 
-public class Simulation {
+public abstract class Simulation {
+
+    private int simRow, simCol;
+    private int simWidth, simHeight;
+    
+    public Simulation(int row, int col, int width, int height){
+        this.simRow = row;
+        this.simCol = col;
+        this.simWidth = width;
+        this.simHeight = height;
+
+    }
+
+    public int getWidth(){return simWidth;}
+    public int getHeight(){return simHeight;}
+    public int getColPos(){return simCol;}
+    public int getRowPos(){return simRow;}
+
+
+    public void updateCell(int x, int y){
+
+    }
+
+    public void updateGrid(){
+
+    }
+
 }
