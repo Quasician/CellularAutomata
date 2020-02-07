95e970b7fe19b90073fb8f81b360b79397859f39
Rodrigo <rea20@duke.edu>
2020-02-07 18:24:07 -0500

    finished createGrid method for SugarSim

--- src/cellsociety/SugarSim.java
+++ src/cellsociety/SugarSim.java
@@ -6,9 +6,9 @@ import java.util.HashMap;
 
 public class SugarSim extends Simulation {
 
-    private int capacity;
-    private int metabolism;
+    private int defaultCapacity;
     private int defaultSugar;
+    private int defaultMetabolism;
     private double percentAgent;
     private double percentSugarFull;
     private double percentSugarHalf;
@@ -23,8 +23,8 @@ public class SugarSim extends Simulation {
     }
 
     public void initParams() {
-        capacity = (int) (getParams().get("capacity") * 10) / 10;
-        metabolism = (int) (getParams().get("metabolism") * 10) / 10;
+        defaultCapacity = (int) (getParams().get("capacity") * 10) / 10;
+        defaultMetabolism = (int) (getParams().get("metabolism") * 10) / 10;
         defaultSugar = (int) (getParams().get("defaultSugar") * 10) / 10;
         percentAgent = getParams().get("percentAgent");
         percentSugarFull = getParams().get("percentSugarFull");
@@ -33,10 +33,32 @@ public class SugarSim extends Simulation {
     }
 
     public void createGrid(int rows, int cols) {
+        createGrid(new String[rows][cols]);
         SugarCell[][] sugarGrid = new SugarCell[rows][cols];
         for(int i = 0; i<getRows();i++) {
             for(int j = 0; j<getCols();j++) {
-                
+                double choice = Math.random();
+
+                if (choice <= percentAgent) {
+                    sugarGrid[i][j] = new SugarCell(i, j, "agent", 0, defaultSugar, defaultMetabolism);
+                    sugarGrid[i][j].setNextState(new SugarCell(i, j, "agent", 0, defaultSugar, defaultMetabolism));
+                    setCell(i,j,"agent");
+                }
+                if (choice <= percentAgent + percentSugarFull) {
+                    sugarGrid[i][j] = new SugarCell(i, j, "sugar_full", defaultCapacity, defaultCapacity, 0);
+                    sugarGrid[i][j].setNextState(new SugarCell(i, j, "sugar_full", defaultCapacity, defaultCapacity, 0));
+                    setCell(i,j,"sugar_full");
+                }
+                if (choice <= percentAgent + percentSugarFull + percentSugarHalf) {
+                    sugarGrid[i][j] = new SugarCell(i, j, "sugar_half", defaultCapacity, defaultCapacity / 2, 0);
+                    sugarGrid[i][j].setNextState(new SugarCell(i, j, "sugar_half", defaultCapacity, defaultCapacity / 2, 0));
+                    setCell(i,j,"sugar_half");
+                }
+                else {
+                    sugarGrid[i][j] = new SugarCell(i, j, "sugar_zero", defaultCapacity, 0, 0);
+                    sugarGrid[i][j].setNextState(new SugarCell(i, j, "sugar_zero", defaultCapacity, 0, 0));
+                    setCell(i,j,"sugar_zero");
+                }
             }
         }
     }
