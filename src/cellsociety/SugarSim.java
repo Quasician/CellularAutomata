79ebfd2909271b88a541949aa8f9832dd38653bd
Rodrigo <rea20@duke.edu>
2020-02-07 18:12:20 -0500

    set up temporary intial params code in SugarSim

--- src/cellsociety/SugarSim.java
+++ src/cellsociety/SugarSim.java
@@ -6,6 +6,15 @@ import java.util.HashMap;
 
 public class SugarSim extends Simulation {
 
+    private int capacity;
+    private int metabolism;
+    private int defaultSugar;
+    private double percentAgent;
+    private double percentSugarFull;
+    private double percentSugarHalf;
+    private double percentSugarZero;
+
+
     public SugarSim(double rows, double cols, int width, int height, HashMap<String, Double> params) {
         super((int) rows, (int) cols, width, height, params);
         initParams();
@@ -14,11 +23,22 @@ public class SugarSim extends Simulation {
     }
 
     public void initParams() {
-
+        capacity = (int) (getParams().get("capacity") * 10) / 10;
+        metabolism = (int) (getParams().get("metabolism") * 10) / 10;
+        defaultSugar = (int) (getParams().get("defaultSugar") * 10) / 10;
+        percentAgent = getParams().get("percentAgent");
+        percentSugarFull = getParams().get("percentSugarFull");
+        percentSugarHalf = getParams().get("percentSugarHalf");
+        percentSugarZero = getParams().get("percentSugarZero");
     }
 
     public void createGrid(int rows, int cols) {
-
+        SugarCell[][] sugarGrid = new SugarCell[rows][cols];
+        for(int i = 0; i<getRows();i++) {
+            for(int j = 0; j<getCols();j++) {
+                
+            }
+        }
     }
 
     public void updateGrid() {
@@ -32,6 +52,8 @@ public class SugarSim extends Simulation {
     public void setUpHashMap() {
         createColorMap(new HashMap<>());
         addToColorMap("agent", "red");
-        addToColorMap("sugar", "yellow");
+        addToColorMap("sugar_full", "gold");
+        addToColorMap("sugar_half", "yellow");
+        addToColorMap("sugar_zero", "lemonchiffon");
     }
 }
