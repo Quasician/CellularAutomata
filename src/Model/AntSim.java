1368c1f0eb92ed52b835cc68c700f83a7157fa11
Rodrigo <rea20@duke.edu>
2020-02-09 14:13:31 -0500

    added abstract cell class and blank methods for AntSim

--- src/Model/AntSim.java
+++ src/Model/AntSim.java
@@ -1,4 +1,28 @@
 package Model;
 
-public class AntSim {
+public class AntSim extends Simulation {
+
+    @Override
+    public void initParams() {
+
+    }
+
+    @Override
+    public void createGrid(int numRows, int numCols) {
+
+    }
+
+    @Override
+    public void updateGrid() {
+
+    }
+
+    public void updateCell() {
+
+    }
+
+    @Override
+    public void setUpHashMap() {
+
+    }
 }
