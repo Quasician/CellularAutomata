29a267c71ff4d22a33c55783708429483f1bf7fc
Rodrigo <rea20@duke.edu>
2020-02-15 02:14:38 -0500

    added javaDoc comments to AntCell class

--- src/Model/AntCell.java
+++ src/Model/AntCell.java
@@ -1,11 +1,40 @@
 package Model;
 
+/**
+ * @author Rodrigo Araujo
+ *
+ * Purpose: A class that extends the abstract cell
+ * class and implements the necessary variables and
+ * methods to create the Ant simulation.
+ *
+ * Assumptions: Inputting the wrong values would cause the cell
+ * class to fail.
+ *
+ * Dependencies: This subclass is dependent on the abstract
+ * cell class.
+ *
+ * Example:
+ *
+ *          AntCell exCell = new AntCell(1, 1, "ant", 0, "food", 0);
+ *
+ */
+
 public class AntCell extends Cell {
 
     protected int pheromone;
     protected int food;
     private String mission;
     private AntCell nextState;
+
+    /**
+     * Purpose: AntCell constructor that defines variables
+     * to be used in the cell class.
+     *
+     * Assumptions: Inputting the wrong values would cause it
+     * to fail.
+     *
+     * Return: N/A
+     */
 
     public AntCell(int x, int y, String name, int pheromone, String mission, int food) {
         super(x, y, name);
@@ -14,24 +43,53 @@ public class AntCell extends Cell {
         this.food = food;
     }
 
-    public void setMission(String input) {this.mission = mission;}
+    /**
+     * Purpose: Method to return the mission state of the ant cell.
+     * This determines the direction the ant moves.
+     *
+     * Assumptions: Calling this method on an object that is not
+     * of the subclass cell would cause it to fail.
+     *
+     * Return: String
+     */
+
     public String getMission() {return mission;}
+
+    /**
+     * Purpose: Method to update the future state of the cell.
+     * This makes it easier to update the cell in the simulation.
+     *
+     * Assumptions: Inputting the wrong values would cause it
+     * to fail or calling this method on an object that is not
+     * of the subclass cell would cause it to fail.
+     *
+     * Return: N/A
+     */
+
     public void setNextState(AntCell input) {this.nextState = input;}
+
+    /**
+     * Purpose: Method to return the future state of the cell.
+     * This makes it easier to update the cell in the simulation.
+     *
+     * Assumptions: Calling this method on an object that is not
+     * of the subclass cell would cause it to fail.
+     *
+     * Return: AntCell
+     */
+
     public AntCell getNextState() {return nextState;}
+
+    /**
+     * Purpose: Method to increase the number of phero in a cell
+     * by a determined input in the simulation.
+     *
+     * Assumptions: Inputting the wrong values would cause it
+     * to fail or calling this method on an object that is not
+     * of the subclass cell would cause it to fail.
+     *
+     * Return: N/A
+     */
+
     public int increasePhero(int input) {return this.pheromone += input;}
-
-
-    private AntCell top(AntCell[][] gridCopy, int x, int y) { return gridCopy[(x+gridCopy.length)%gridCopy.length][(y+1+gridCopy[0].length)%gridCopy[0].length];}
-    private AntCell bottom(AntCell[][] gridCopy, int x, int y) { return gridCopy[(x+gridCopy.length)%gridCopy.length][(y-1+gridCopy[0].length)%gridCopy[0].length];}
-    private AntCell left(AntCell[][] gridCopy, int x, int y) { return gridCopy[(x-1+gridCopy.length)%gridCopy.length][(y+gridCopy[0].length)%gridCopy[0].length];}
-    private AntCell right(AntCell[][] gridCopy, int x, int y) { return gridCopy[(x+1+gridCopy.length)%gridCopy.length][(y+gridCopy[0].length)%gridCopy[0].length];}
-
-    public AntCell[] get4Neighbors(int x, int y, AntCell[][] gridCopy) {
-        AntCell[] neighbors = new AntCell[4];
-        neighbors[0] = top(gridCopy, x, y);
-        neighbors[1] = bottom(gridCopy, x, y);
-        neighbors[2] = left(gridCopy, x, y);
-        neighbors[3] = right(gridCopy, x, y);
-        return neighbors;
-    }
 }
